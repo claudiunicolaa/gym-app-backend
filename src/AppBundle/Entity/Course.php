3c9699a814ccb2c7b736233f76706cd7e88f3f6c
Andu <a.pope95@yahoo.com>
2017-10-24 14:58:07 +0300

    [GYM-5]Updated Course entity and created Course Repo

--- src/AppBundle/Entity/Course.php
+++ src/AppBundle/Entity/Course.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Entity;
 
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use ApiPlatform\Core\Annotation\ApiResource;
@@ -10,6 +11,8 @@ use ApiPlatform\Core\Annotation\ApiResource;
  * @author Alexandru Emil Popa <a.pope95@yahoo.com>
  *
  * @ORM\Entity
+ * @ORM\Table(name="courses")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CourseRepository")
  */
 class Course
 {
@@ -24,18 +27,86 @@ class Course
      * @ORM\ManyToOne(targetEntity="User")
      * @ORM\JoinColumn(name="trainer_id", referencedColumnName="id")
      */
-    private $trainer;
+    protected $trainer;
 
     /**
-     * @var
-     * @ORM\Column(name="event_date", type="datetime", nullable=false)
+     * @ORM\Column(name="event_date", type="datetime")
      */
     protected $eventDate;
 
     /**
-     * @var
-     * @ORM\Column(name="capacity", type="integer", nullable=false)
+     * @ORM\Column(name="capacity", type="integer")
      */
     protected $capacity;
-}
 
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return User | null
+     */
+    public function getTrainer()
+    {
+        if(isset($this->trainer))
+            return $this->trainer;
+
+        return null;
+    }
+
+    /**
+     * @param User $trainer | null
+     * @return $this
+     */
+    public function setTrainer($trainer)
+    {
+        if(isset($trainer))
+            $this->trainer = $trainer;
+
+        return $this;
+    }
+
+    /**
+     * @return datetime
+     */
+    public function getEventDate()
+    {
+        return $this->eventDate;
+    }
+
+    /**
+     * @param datetime $eventDate | null
+     * @return $this
+     */
+    public function setEventDate($eventDate)
+    {
+        if(isset($eventDate))
+            $this->eventDate = $eventDate;
+
+        return $this;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getCapacity()
+    {
+        return $this->capacity;
+    }
+
+    /**
+     * @param integer $capacity | null
+     * @return $this
+     */
+    public function setCapacity($capacity)
+    {
+        if(isset($capacity))
+            $this->capacity = $capacity;
+
+        return $this;
+    }
+}
---
+++ src/AppBundle/Repository/CourseRepository.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andu
+ * Date: 24.10.2017
+ * Time: 14:49
+ */
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class CourseRepository extends EntityRepository
+{
+
+}
