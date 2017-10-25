b06279748fb932c6d83e44ab33ee8d5449903a06
Johnny <i.ovidiuenache@yahoo.com>
2017-10-26 00:49:49 +0300

    [GYM-33] Refactoring

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -35,6 +35,40 @@ class ProductController extends Controller
      *         }
      *     }
      *
+     * @Route("/api/products", name="products_get", methods={"GET"})
+     *
+     * @return JsonResponse
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Returns the list of products.",
+     *  section="Product",
+     *  statusCodes={
+     *      200="Returned when successful",
+     *      401="Returned when the request is valid, but the token given is invalid or missing"
+     *  }
+     *  )
+     */
+    public function getProductsAction() : JsonResponse
+    {
+        /** @var ProductRepository $productRepository */
+        $productRepository = $this->get(ProductRepository::class);
+
+        return new JsonResponse($productRepository->findAll(), 200);
+    }
+
+    /**
+     * ### Example Response ###
+     *      {
+     *         {
+     *              "id" : "1",
+     *              "price" : "45",
+     *              "description" : "Example description #1",
+     *              "image" : "https://i.imgur.com/NiCqGa3.jpg",
+     *              "name" : "Example Product B"
+     *         }
+     *     }
+     *
      * @Route("/api/product", name="product_get", methods={"GET"})
      *
      * @param Request $request
@@ -43,28 +77,28 @@ class ProductController extends Controller
      *
      * @ApiDoc(
      *  resource=true,
-     *  description="Returns a list of products that match the given filters.",
+     *  description="Returns a product with given id.",
      *  section="Product",
      * filters={
-     *      {"name"="id", "dataType"="int", "description"="Returns the product with the given id. Optional"},
+     *      {"name"="id", "dataType"="int", "description"="Returns the product with the given id. Mandatory"},
      *  },
      *  statusCodes={
      *      200="Returned when successful",
+     *      400="Returned when the request is invalid",
      *      401="Returned when the request is valid, but the token given is invalid or missing"
      *  }
      *  )
      */
-    public function getProductsAction(Request $request) : JsonResponse
+    public function getProductAction(Request $request) : JsonResponse
     {
         $productId = $request->get('id');
+        if ($productId === null) {
+            return new JsonResponse(['error' => 'Missing required parameter id'], 400);
+        }
 
         /** @var ProductRepository $productRepository */
         $productRepository = $this->get(ProductRepository::class);
 
-        if ($productId !== null) {
-            return new JsonResponse($productRepository->find($productId), 200);
-        }
-
-        return new JsonResponse($productRepository->findAll(), 200);
+        return new JsonResponse($productRepository->find($productId), 200);
     }
 }
