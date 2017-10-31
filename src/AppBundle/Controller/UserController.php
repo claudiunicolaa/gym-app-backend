602386c682b609ef91f4443c0c12cf546119a978
Andu <a.pope95@yahoo.com>
2017-10-31 23:52:57 +0200

    [GYM-16] Created the user registration action

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -75,5 +76,77 @@ class UserController extends Controller
     public function updateUserAction(Request $request) : JsonResponse
     {
         throw new NotImplementedException("Not implemented");
+    }
+
+    /**
+     * ### Example Response ###
+     *      {
+     *         "id" : "1",
+     *         "username" : "smith_adam",
+     *         "email" : "smithadam@gmail.com"
+     *     }
+     *
+     * @todo Implement this method
+     *
+     * @Route("/api/user", name="user_post", methods={"POST"})
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Used to register a user",
+     *  section="User",
+     *  statusCodes={
+     *      200="Returned when successful",
+     *      400="Returned when the request is not valid",
+     *      401="Returned when the request is valid, but the email or username given already exist in the database"
+     *  }
+     *  )
+     */
+    public function registerUserAction(Request $request) : JsonResponse
+    {
+        $username   = $request->get('username');
+        $password   = $request->get('password');
+        $email      = $request->get('email');
+        $firstName  = $request->get('firstName');
+        $lastName   = $request->get('lastName');
+        $picture    = $request->get('picture');
+
+        if (!isset($email) || !isset($password) || !isset($username)) {
+            return new JsonResponse(['error' => 'Missing email, password or username'], 400);
+        }
+
+        $possibleUserByEmail = $this->get('fos_user.user_manager')->findUserByEmail($email);
+        $possibleUserByUsername = $this->get('fos_user.user_manager')->findUserByUsername($username);
+
+        if ($possibleUserByEmail || $possibleUserByUsername) {
+            return new JsonResponse(['error' => 'User already exists'], 401);
+        }
+
+        $userManager    = $this->get('fos_user.user_manager');
+        $user           = $userManager->createUser();
+
+        $user->setUsername($username);
+        $user->setEmail($email);
+        $user->setFirstName($firstName);
+        $user->setLastName($lastName);
+        $user->setPicture($picture);
+        $user->setPassword($password);
+        $user->addRole("ROLE_USER");
+        $user->setEnabled(true);
+
+        $userManager->updateUser($user);
+
+        return new JsonResponse(
+            [
+                'id' => $user->getId(),
+                'username' => $username,
+                'email' => $email
+            ],
+            200
+        );
+
     }
 }
