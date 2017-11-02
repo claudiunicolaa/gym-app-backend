32d3123a06e0fc8e6c9c798b837ec97ae2fdc410
Johnny <i.ovidiuenache@yahoo.com>
2017-11-02 20:00:54 +0200

    [GYM-37] Refactored register route.

--- src/AppBundle/Controller/AuthorizationController.php
+++ src/AppBundle/Controller/AuthorizationController.php
@@ -2,12 +2,16 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use AppBundle\Exception\UserValidationException;
+use AppBundle\Repository\UserRepository;
+use AppBundle\Services\Validator\UserValidator;
+use FOS\UserBundle\Model\UserManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 /**
  * Class AuthorizationController
@@ -74,13 +78,61 @@ class AuthorizationController extends Controller
     }
 
     /**
+     * @Route("/api/register", name="user_register", methods={"POST"})
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Used to register a user.",
+     *  section="Authorization",
+     *  filters={
+     *      {"name"="email", "dataType"="string", "description" : "Mandatory"},
+     *      {"name"="password", "dataType"="string", "description" : "Mandatory"},
+     *      {"name"="fullName", "dataType"="string", "description": "Mandatory. Format: last_name first_name"},
+     *      {"name"="picture", "dataType"="string", "description" : "Optional"}
+     *  },
+     *  statusCodes={
+     *      200="Returned when successful",
+     *      400="Returned when the request is not valid"
+     *  }
+     *  )
+     */
+    public function registerUserAction(Request $request) : JsonResponse
+    {
+        $queryParams = $request->query->all();
+        $userValidator = $this->get(UserValidator::class);
+        try {
+            $userValidator->checkMandatoryFields($queryParams);
+            $userValidator->validate($queryParams);
+        } catch (UserValidationException $ex) {
+            return new JsonResponse(['error' => $ex->getMessage()], 400);
+        }
+
+        $userRepository = $this->getDoctrine()->getRepository(User::class);
+        if ($userRepository->findOneBy(['email' => $queryParams['email']]) instanceof User) {
+            return new JsonResponse(['error' => 'User with given email already exists!'], 400);
+        }
+
+        /** @var UserManager $userManager */
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->createUser();
+        $this->setUserProperties($user, $queryParams);
+        $userManager->updateUser($user);
+
+        return new JsonResponse('', 200);
+    }
+
+    /**
      * Returns the highest user role
      *
      * @param User $user
      *
      * @return string
      */
-    protected function getHighestRole(User $user) : string
+    private function getHighestRole(User $user) : string
     {
         $userRoles = $user->getRoles();
         $rolesSortedByImportance = ['ROLE_ADMIN', 'ROLE_TRAINER'];
@@ -93,5 +145,21 @@ class AuthorizationController extends Controller
         }
 
         return 'ROLE_USER';
+    }
+
+    /**
+     * @param User  $user
+     * @param array $data
+     */
+    private function setUserProperties(User $user, array $data)
+    {
+        $user->setEmail($data['email']);
+        $user->setUsername($user->getEmail());
+        $user->setLastName(explode(' ', $data['fullName'])[0]);
+        $user->setFirstName(explode(' ', $data['fullName'])[1] ?? '');
+        $user->setPicture($data['picture'] ?? '');
+        $user->setPlainPassword($data['password']);
+        $user->addRole("ROLE_USER");
+        $user->setEnabled(true);
     }
 }
---
+++ src/AppBundle/Exception/UserValidationException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\Exception;
+
+/**
+ * Class UserValidationException
+ *
+ * @author Ioan Ovidiu Enache <i.ovidiuenache@yahoo.com>
+ */
+class UserValidationException extends \Exception
+{
+}
