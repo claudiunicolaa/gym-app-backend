1c723ba6f84051441547e092fdb42e2fa63d32cf
Johnny <i.ovidiuenache@yahoo.com>
2017-12-01 23:35:11 +0200

    [GYM-55] Can now create products with image file attached from admin.

---
+++ src/AppBundle/Validator/Constraints/ImageExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ *
+ * @author Ioan Ovidiu Enache <i.ovidiuenache@yahoo.com>
+ */
+class ImageExtension extends Constraint
+{
+}
