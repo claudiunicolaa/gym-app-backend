830a920b2909bd29dd6563426e2627a837bfbf1d
Andu <a.pope95@yahoo.com>
2017-12-20 20:42:58 +0200

    [GYM-70]Added photo delete route and implemented ajax call to remove photo

--- src/AppBundle/Controller/MediaController.php
+++ src/AppBundle/Controller/MediaController.php
@@ -3,10 +3,15 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Filesystem\Exception\IOException;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Finder\Finder;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Acl\Exception\Exception;
 
 /**
@@ -80,5 +85,43 @@ class MediaController extends Controller
             'default/gallery.html.twig',
             array('photos' => $fileNames)
         );
+    }
+
+    /**
+     * @Route("/admin/photos", name="delete_picture", methods={"DELETE"})
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Delete the picture with the given ID",
+     *  section="Media",
+     *  statusCodes={
+     *      200="Returned when successful",
+     *      400="Returned when the request is invalid",
+     *      401="Returned when the request is valid, but the token given is invalid or missing",
+     *      405="Returned when the method called is not allowed",
+     *  }
+     *  )
+     */
+    public function deletePictureAction(Request $request) : JsonResponse
+    {
+        $id = $request->get('id');
+        if (null === $id) {
+            return new JsonResponse(['error' => 'Picture with given id doesn\'t exist'], 400);
+        }
+
+        $photoPath = $this->getParameter('kernel.root_dir') . self::PATH_TO_GYM_PHOTOS;
+
+        try {
+            $fileSystem = new Filesystem();
+            $file = new File($photoPath . '/' . $id);
+
+            $fileSystem->remove($file);
+        } catch (FileNotFoundException|IOException $ignored) {
+            return new JsonResponse($ignored->getMessage(), 400);
+        }
+
+        return new JsonResponse('', 200);
     }
 }
